324b9dff3c577fde4050e0cf840a2a0de266f019
Wei Liu <liuweixa@redhat.com>
2024-06-30 20:48:17 +0800

    perfermance test
    Signed-off-by: Wei Liu <liuweixa@redhat.com>

--- test/performance/pkg/hub/workloads/workloads.go
+++ test/performance/pkg/hub/workloads/workloads.go
@@ -3,10 +3,12 @@ package workloads
 import (
 	"bytes"
 	"embed"
+	"fmt"
 	"text/template"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/rand"
 	"sigs.k8s.io/yaml"
 
 	workv1 "open-cluster-management.io/api/work/v1"
@@ -16,25 +18,34 @@ import (
 var ManifestFiles embed.FS
 
 type RenderConfig struct {
+	WorkName    string
 	ClusterName string
 }
 
-var workloadFiles = map[string]string{
-	"busybox": "manifests/deployment.yaml",
+var workloadFiles = []string{
+	"manifests/configmap.yaml",
+	"manifests/deployment.yaml",
+	"manifests/role.yaml",
+	"manifests/rolebinding.yaml",
+	"manifests/secret.yaml",
+	"manifests/serviceaccount.yaml",
 }
 
 func ToManifestWorks(clusterName string, workType string) ([]*workv1.ManifestWork, error) {
 	works := []*workv1.ManifestWork{}
 
-	for name, file := range workloadFiles {
-		raw, err := render(file, &RenderConfig{ClusterName: clusterName})
+	for _, file := range workloadFiles {
+		raw, err := render(file, &RenderConfig{
+			WorkName:    fmt.Sprintf("%s-%s", clusterName, workType),
+			ClusterName: clusterName,
+		})
 		if err != nil {
 			return nil, err
 		}
 
 		works = append(works, &workv1.ManifestWork{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      name,
+				Name:      fmt.Sprintf("%s-%s", clusterName, rand.String(10)),
 				Namespace: clusterName,
 				Labels: map[string]string{
 					"maestro.performance.test": workType,
